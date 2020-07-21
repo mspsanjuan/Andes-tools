4a6c5246635a1b2b8a2ef182a2660cc9e617a19f
Carlos Alexis Yacante <carlitox.yacante@gmail.com>
2020-07-21 09:18:56 -0300

    feat: Auth de permisos para acceder a ServSalud

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -10,19 +10,8 @@ import { LABELS } from '../../styles/properties';
 })
 export class InicioComponent implements AfterViewInit {
     @HostBinding('class.plex-layout') layout = true;
-    public turnos = '';
-    public mpi = '';
-    public rup = '';
-    public internacion = '';
-    public internacionEpicrisis = '';
-    public solicitudes = '';
-    public prestamosHC = '';
-    public dashboard = false;
-    public analytics = '';
-    public usuarios = '';
+    public servSalud = '';
     public denied = false;
-    public accessList: any = [];
-    public provincia = LABELS.provincia;
 
 
     constructor(public auth: Auth, public appComponent: AppComponent, private plex: Plex) { }
@@ -30,56 +19,9 @@ export class InicioComponent implements AfterViewInit {
     ngAfterViewInit() {
         window.setTimeout(() => {
             this.denied = true;
-            if (this.auth.getPermissions('turnos:?').length > 0) {
-                if (this.auth.getPermissions('turnos:planificarAgenda:?').length > 0) {
-                    this.turnos = 'gestor';
-                } else {
-                    if (this.auth.getPermissions('turnos:darTurnos:?').length > 0) {
-                        this.turnos = 'inicioTurnos';
-                    }
-                }
-                // this.turnos = 'turnos';
-                this.denied = false;
-            }
 
-            if (this.auth.getPermissions('mpi:?').length > 0) {
-                this.mpi = 'mpi';
-                this.denied = false;
-            }
-
-            if (this.auth.getPermissions('analytics:?').length > 0) {
-                this.analytics = 'analytics';
-                this.denied = false;
-            }
-
-            if (this.auth.getPermissions('rup:?').length > 0) {
-                this.rup = 'rup';
-                this.denied = false;
-            }
-
-            if (this.auth.check('internacion:mapaDeCamas')) {
-                this.internacion = 'internacion';
-                this.denied = false;
-            }
-            if (this.auth.check('internacion:inicio')) {
-                this.internacionEpicrisis = 'internacionEpicrisis';
-                this.denied = false;
-            }
-
-            if (this.auth.getPermissions('solicitudes:?').length > 0) {
-                this.solicitudes = 'solicitudes';
-            }
-
-            if (this.auth.check('prestamos:?')) {
-                this.prestamosHC = 'prestamosHC';
-                this.denied = false;
-            }
-
-            if (this.auth.check('dashboard:citas:ver') || this.auth.check('dashboard:top:ver')) {
-                this.dashboard = true;
-            }
-            if (this.auth.getPermissions('usuarios:?').length > 0) {
-                this.usuarios = 'usuarios';
+            if (this.auth.getPermissions('tools:servSalud:?').length > 0 ) {
+                this.servSalud = 'servSalud';
                 this.denied = false;
             }
         });
